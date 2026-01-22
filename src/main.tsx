e930824e1c8f0f3ab61fc6f7994651fbaaaa7552
Roman Galankin <5427611@gmail.com>
2026-01-23 02:13:48 +0300

    [Blink] Debugging blank screen issue - temporarily simplified App component to isolate the problem

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Toaster } from 'sonner'
-import { BlinkProvider, BlinkAuthProvider } from '@blinkdotnew/react'
-import { LanguageProvider } from './lib/i18n'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BlinkProvider 
-      projectId={import.meta.env.VITE_BLINK_PROJECT_ID}
-      publishableKey={import.meta.env.VITE_BLINK_PUBLISHABLE_KEY}
-    >
-      <BlinkAuthProvider>
-        <LanguageProvider>
-          <Toaster position="top-right" richColors />
-          <App />
-        </LanguageProvider>
-      </BlinkAuthProvider>
-    </BlinkProvider>
+    <App />
   </React.StrictMode>,
 )
